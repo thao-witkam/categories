961ebdc611d391154aff8e606ff97dc0d9e389d4
thao <nguyen.hongthao@gmail.com>
2018-09-08 19:38:14 +0200

    test

--- tests/Feature/CreateCategoryTest.php
+++ tests/Feature/CreateCategoryTest.php
@@ -1,7 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Thao Nguyen
- * Date: 9/8/2018
- * Time: 4:54 PM
- */
+namespace Test\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+Class CreateCategoryTest extends \Tests\TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function guest_can_create_category()
+    {
+        $response = $this->post('/cat', [
+            'parentId' => 0,
+            'name' => 'Category-name',
+            'description' => 'some description'
+        ]);
+
+        $this->assertDatabaseHas('category', [
+            'name' => 'Category-name'
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertHeader('Location', url('cat'));
+
+        $this->get('/cat')->assertSee('Category-name');
+    }
+
+    /** @test */
+    public function not_create_if_invalid_data()
+    {
+        $response = $this->post('/cat', ['parentId', 'name', 'description']);
+        $response->assertSessionHasErrors(['parentId']);
+    }
+
+    /** @test */
+    public function not_create_if_wrong_type_data()
+    {
+        $response = $this->post('/cat', [
+            'parentId' => 'string',
+            'name' => 3,
+            'description' => 3
+        ]);
+
+        $response->assertSessionHasErrors(['parentId', 'name', 'description']);
+    }
+}
